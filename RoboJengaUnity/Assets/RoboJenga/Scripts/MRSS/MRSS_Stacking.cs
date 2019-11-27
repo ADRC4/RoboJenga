0248dafe802b1e6cb789fddcc67fa158d70ba9aa
mukul gupta <mukul.gupta2309@outlook.com>
2019-11-27 14:03:11 +0000

    MRSS

---
+++ RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_Stacking.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MRSS_Stacking : IStackable
+{
+    public string Message { get; private set; }
+
+
+    public ICollection<Pose> Display { get; } = new List<Pose>();
+
+    readonly Vector3 _placePoint = new Vector3(1.4f, 0.045f, 0.8f);
+
+    readonly Vector3 _tileSize = new Vector3(0.18f, 0.045f, 0.06f);
+
+
+    readonly Rect _rectTarget;
+    readonly Rect _PickPileRect;
+
+
+    readonly float _gap = 0.01f;
+    readonly ICamera _camera;
+
+
+    int _tileCount = 1;
+    int _tilelayer = 1;
+    int _layerlength = 5;
+
+
+    List<Pose> _pickTiles = new List<Pose>();
+    IList<Pose> _existingBlocks = new List<Pose>();
+
+    public IList<Pose> targetTower;
+
+    public MRSS_Stacking(Mode mode)
+    {
+        if (mode == Mode.Virtual)
+            _camera = new MRSS_VirtualCamera();
+        else
+            _camera = new MotiveCamera();
+
+        Message = "Running MRSS stacking.";
+
+        float m = 0.02f;
+
+        //Split the table into two.
+        _PickPileRect = new Rect(0.8f + m, 0 + m, 0.6f - m * 2, 0.8f - m * 2);
+        _rectTarget = new Rect(0 + m, 0 + m, 0.8f - m * 2, 0.8f - m * 2);
+    }
+
+    public PickAndPlaceData GetNextTargets()
+    {
+        // Top layer on the pickSide
+        var topLayer = _camera.GetTiles(_PickPileRect);
+
+        targetTower = _camera.GetTiles(_rectTarget);
+
+        if (topLayer == null)
+        {
+            Message = "Camera error.";
+            return null;
+        }
+
+        if (topLayer.Count == 0)
+        {
+            return null;
+        }
+
+
+        var pick = topLayer.First();
+        _pickTiles.Add(pick);
+
+       
+
+        
+        var place = ConstructLocation(_tileCount);
+
+
+        if (_tileCount < _layerlength) { _tileCount++; }
+        else { _tilelayer++; _tileCount = 1; _layerlength--; }
+
+        
+
+        Display.Add(place);
+        return new PickAndPlaceData { Pick = pick, Place = place };
+    }
+
+
+
+
+
+    Pose ConstructLocation(int index)
+    {
+
+
+
+
+        Debug.Log(targetTower.First<Pose>().position);
+
+        var rotation = Quaternion.Euler(0, 0, 0) * Quaternion.Euler(0, 180f, 0);
+
+        Vector3 position =  new Vector3((targetTower.First<Pose>().position.x) + _tileSize.x*index, _tileSize.y*_tilelayer, targetTower.First<Pose>().position.z) ;
+        
+        return new Pose( position , rotation);
+    }
+
+
+
+   
+}
---
+++ RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_VirtualCamera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class MRSS_VirtualCamera : ICamera
+{
+    Queue<Pose[]> _pickSequence;
+    Queue<Pose[]> _placeSequence;
+
+    public MRSS_VirtualCamera()
+    {
+        var t = new[]
+        {
+           Extensions.PoseFromRotation(0.1f,0.045f,0.4f,90.0f),
+           Extensions.PoseFromRotation(0.1f,0.045f,0.7f,90.0f),
+           Extensions.PoseFromRotation(0.1f,0.045f,0.6f,90.0f),
+           Extensions.PoseFromRotation(0.1f,0.045f,0.2f,90.0f),
+        
+        };
+
+        var v = new[]
+       {
+          
+           Extensions.PoseFromRotation(0.8f,0.045f,0.3f,90.0f),
+         
+        };
+
+        _pickSequence = new Queue<Pose[]>(new[]
+        {
+            new[] {v[0]},
+            new[] {v[0]},new[] {v[0]},new[] {v[0]},new[] {v[0]},new[] {v[0]}
+
+
+        });
+
+        _placeSequence = new Queue<Pose[]>(new[]
+        {
+           
+
+            new[] {t[0],t[1],t[2]},
+           new[] {t[1],t[2]},
+           new[] {t[2]},
+           new Pose[0]
+        });
+
+
+    }
+
+    public IList<Pose> GetTiles(Rect area)
+    {
+        if (area.x <= 0.8f)
+            return _pickSequence.Dequeue();
+
+        else  return _placeSequence.Dequeue();
+
+       
+    }
+}
