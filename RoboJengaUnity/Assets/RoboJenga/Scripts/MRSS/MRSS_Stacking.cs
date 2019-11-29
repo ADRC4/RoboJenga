118dcabb4ab909f7397173daa79b404faa1613e6
mukul gupta <mukul.gupta2309@outlook.com>
2019-11-29 16:12:57 +0000

    Motive 2019 11 28

--- RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_Stacking.cs
+++ RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_Stacking.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography;
+using System;
 
 public class MRSS_Stacking : IStackable
 {
@@ -9,13 +11,14 @@ public class MRSS_Stacking : IStackable
 
     public ICollection<Pose> Display { get; } = new List<Pose>();
 
-    readonly Vector3 _placePoint = new Vector3(1.4f, 0.045f, 0.8f);
+    //readonly Vector3 _placePoint = new Vector3(1.4f, 0.045f, 0.8f);
 
     readonly Vector3 _tileSize = new Vector3(0.18f, 0.045f, 0.06f);
 
 
     readonly Rect _rectTarget;
     readonly Rect _PickPileRect;
+    readonly Rect _rectStart;
 
 
     readonly float _gap = 0.01f;
@@ -24,14 +27,17 @@ public class MRSS_Stacking : IStackable
 
     int _tileCount = 1;
     int _tilelayer = 1;
-    int _layerlength = 5;
 
+    int _layerlengthx;
+    int _layerlengthz;
+    int _layerlength;
+    int _step = 1;
 
     List<Pose> _pickTiles = new List<Pose>();
-    IList<Pose> _existingBlocks = new List<Pose>();
 
-    public IList<Pose> targetTower;
 
+    Pose targetTower;
+    Pose sourceTower;
     public MRSS_Stacking(Mode mode)
     {
         if (mode == Mode.Virtual)
@@ -44,8 +50,12 @@ public class MRSS_Stacking : IStackable
         float m = 0.02f;
 
         //Split the table into two.
-        _PickPileRect = new Rect(0.8f + m, 0 + m, 0.6f - m * 2, 0.8f - m * 2);
-        _rectTarget = new Rect(0 + m, 0 + m, 0.8f - m * 2, 0.8f - m * 2);
+        _PickPileRect = new Rect(1.1f + m, 0 + m, 0.3f - m * 2, 0.8f - m * 2);
+
+        _rectTarget = new Rect(0 + m, 0 + m, 0.6f - m * 2, 0.8f - m * 2);
+        _rectStart = new Rect(0.6f + m, 0 + m, 0.5f - m * 2, 0.8f - m * 2);
+
+
     }
 
     public PickAndPlaceData GetNextTargets()
@@ -53,7 +63,6 @@ public class MRSS_Stacking : IStackable
         // Top layer on the pickSide
         var topLayer = _camera.GetTiles(_PickPileRect);
 
-        targetTower = _camera.GetTiles(_rectTarget);
 
         if (topLayer == null)
         {
@@ -63,48 +72,121 @@ public class MRSS_Stacking : IStackable
 
         if (topLayer.Count == 0)
         {
+            Message = "No tiles to pick.";
             return null;
         }
-
 
         var pick = topLayer.First();
         _pickTiles.Add(pick);
 
-       
 
-        
-        var place = ConstructLocation(_tileCount);
+        //tower
+        var towerBlockTarget = _camera.GetTiles(_rectTarget) ;
+        var towerBlockSource = _camera.GetTiles(_rectStart);
 
 
-        if (_tileCount < _layerlength) { _tileCount++; }
-        else { _tilelayer++; _tileCount = 1; _layerlength--; }
+        if (towerBlockTarget == null || towerBlockSource == null)
+        {
+            Message = "Camera error.";
+            return null;
+        }
 
-        
+        if (towerBlockTarget.Count == 0 || towerBlockSource.Count == 0)
+        {
+            Message = "No tower found.";
+            return null;
+        }
+
+        targetTower = towerBlockTarget.First();
+        sourceTower = towerBlockSource.First();
+
+
+        Display.Add(sourceTower);
+        Display.Add(targetTower);
+
+
+        //_layerlengthx = Mathf.FloorToInt(( ) / _tileSize.x);
+
+        //if(sourceTower.position.z > targetTower.position.z)
+        //    _layerlengthz = Mathf.FloorToInt((  ) / _tileSize.z);
+        //else if(sourceTower.position.z < targetTower.position.z)
+        //    _layerlengthz = Mathf.FloorToInt((targetTower.position.z - sourceTower.position.z) / _tileSize.z);
+
+
+
+
+        _layerlength = Mathf.FloorToInt((Mathf.Sqrt( (sourceTower.position.x - targetTower.position.x)* (sourceTower.position.x - targetTower.position.x) +
+            (sourceTower.position.z - targetTower.position.z)* (sourceTower.position.z - targetTower.position.z))) / (_tileSize.x - _tileSize.z*0.5f));
+
+        Debug.Log(_layerlength);
+
+
+        var place = ConstructLocation(_tileCount );
+
+
+        if (_tileCount < _layerlength - _step)
+        {
+            _tileCount++;
+        }
+        else
+        {
+            if (_tileSize.y * _tilelayer < targetTower.position.y)
+            {
+                _tilelayer++;
+            }
+            else
+            {
+                _tilelayer += 0;
+
+                return null;
+            }
+
+            _tileCount = 1;
+            //_layerlengthx--;
+            _step++;
+        }
+
 
         Display.Add(place);
+
         return new PickAndPlaceData { Pick = pick, Place = place };
     }
-
-
 
 
 
     Pose ConstructLocation(int index)
     {
 
+        Debug.Log(targetTower.position);
 
 
+        int  angle = index % 2 == 0 ? 45 : -45;
+        float sign = _tilelayer % 2 == 0 ? 1: -1;
 
-        Debug.Log(targetTower.First<Pose>().position);
 
-        var rotation = Quaternion.Euler(0, 0, 0) * Quaternion.Euler(0, 180f, 0);
+      
 
-        Vector3 position =  new Vector3((targetTower.First<Pose>().position.x) + _tileSize.x*index, _tileSize.y*_tilelayer, targetTower.First<Pose>().position.z) ;
+        Debug.Log(_layerlengthz);
+
+        //Vector3 position = new Vector3(
+        //    (targetTower.position.x) + _tileSize.x * index,
+        //    _tileSize.y * _tilelayer,
+        //    targetTower.position.z);
+
+        float x = targetTower.position.x + ((sourceTower.position.x - targetTower.position.x) * index / _layerlength);
+        float z = targetTower.position.z + ((sourceTower.position.z - targetTower.position.z) * index / _layerlength);
+
+       
+            Vector3 position = new Vector3(x , _tileSize.y * _tilelayer, z );
+            var rotation = Quaternion.LookRotation(new Vector3(targetTower.position.x, 0, targetTower.position.z), Vector3.up) * Quaternion.Euler(0, sign * angle, 0);
+
+            return new Pose(position, rotation);
+
+  
         
-        return new Pose( position , rotation);
+
+
+
     }
 
-
-
-   
 }
--- RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_VirtualCamera.cs
+++ RoboJengaUnity/Assets/RoboJenga/Scripts/MRSS/MRSS_VirtualCamera.cs
@@ -8,54 +8,104 @@ using UnityEngine;
 class MRSS_VirtualCamera : ICamera
 {
     Queue<Pose[]> _pickSequence;
-    Queue<Pose[]> _placeSequence;
+    Queue<Pose[]> _targetSequence;
+    Queue<Pose[]> _SourceSequence;
 
     public MRSS_VirtualCamera()
     {
         var t = new[]
         {
-           Extensions.PoseFromRotation(0.1f,0.045f,0.4f,90.0f),
-           Extensions.PoseFromRotation(0.1f,0.045f,0.7f,90.0f),
-           Extensions.PoseFromRotation(0.1f,0.045f,0.6f,90.0f),
-           Extensions.PoseFromRotation(0.1f,0.045f,0.2f,90.0f),
-        
+           Extensions.PoseFromRotation(1.3f,0.135f,0.4f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.135f,0.7f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.135f,0.6f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.135f,0.2f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.09f,0.4f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.09f,0.7f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.09f,0.6f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.09f,0.2f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.045f,0.4f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.045f,0.7f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.045f,0.6f,0.0f),
+           Extensions.PoseFromRotation(1.3f,0.045f,0.2f,0.0f)
+
+        };
+
+        var u = new[]
+    {
+
+           Extensions.PoseFromRotation(1.0f,0.045f,0.4f,90.0f),
+
         };
 
         var v = new[]
        {
-          
-           Extensions.PoseFromRotation(0.8f,0.045f,0.3f,90.0f),
-         
+
+           Extensions.PoseFromRotation(0.2f,0.27f,0.4f,90.0f),
+
         };
 
         _pickSequence = new Queue<Pose[]>(new[]
         {
-            new[] {v[0]},
-            new[] {v[0]},new[] {v[0]},new[] {v[0]},new[] {v[0]},new[] {v[0]}
 
+           new[] {t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[4],t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[5],t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[6],t[7],t[8],t[9],t[10]},
+           new[] {t[7],t[8],t[9],t[10]},
+           new[] {t[8],t[9],t[10]},
+           new[] {t[9],t[10]},
+           new[] {t[10]},
 
-        });
-
-        _placeSequence = new Queue<Pose[]>(new[]
-        {
-           
-
-            new[] {t[0],t[1],t[2]},
-           new[] {t[1],t[2]},
-           new[] {t[2]},
            new Pose[0]
+
         });
 
+        _targetSequence = new Queue<Pose[]>(new[]
+        {
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]},
+            new[] {v[0]}
+        });
+
+        _SourceSequence = new Queue<Pose[]>(new[]
+  {
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+          new[] {u[0]},
+
+        });
 
     }
 
     public IList<Pose> GetTiles(Rect area)
     {
-        if (area.x <= 0.8f)
+        if (area.x >= 1.1f)
             return _pickSequence.Dequeue();
 
-        else  return _placeSequence.Dequeue();
+        else if (area.x < 1.1f && area.x > 0.6f)
+            return _SourceSequence.Dequeue();
+        else if (area.x <= 0.6f)
+            return _targetSequence.Dequeue();
+        else return null;
 
-       
     }
 }
